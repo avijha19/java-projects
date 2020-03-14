ff4f61cc7501b5b5e7ee250a8cb4c71cd401532c
Avinash Kumar <avinash.kumar@macys.com>
2020-03-13 18:31:02 -0700

    new changes

---
+++ JavaAlgorithms/src/LeetCode/Tree/BinaryTreeTraversal.java
@@ -0,0 +1,22 @@
+package LeetCode.Tree;
+
+public class BinaryTreeTraversal {
+	
+	
+	static TreeNode root;
+
+
+	public static void main(String[] args) {
+		
+		
+
+
+	}
+	
+	
+	private void insert(int num) { 
+		
+		
+	}
+
+}
